424ae486ee3540821fba6e851d10bb437f97a508
Ashwani <ashwani.sinha2@gmail.com>
2021-05-13 22:41:26 +0530

    Wrap title into section

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import plane from '../../assets/img/plane.gif';
-import './Style.scss';
+import React from "react";
+import plane from "../../assets/img/plane.gif";
+import "./Style.scss";
 
-const Introduction=()=>{
-    return(
-        <div className='main'>
-            <img src={plane} alt="plane.gif"/>
-            <h1 className="heading">Ashwani Sinha</h1>
-            <button className='Connect-btn'>Connect with me!</button>
-        </div>
-    )
-}
+const Introduction = () => {
+  return (
+    <div className="main">
+      <section className="intro">
+        <h1>
+          <img src={plane} alt="plane" /><br/>
+          <span className="title">Ashwani Sinha</span><br/>
+        </h1>
+      </section>
+      <span className="intro-text">I am a</span><br/>
+      <button className="Connect-btn">Connect with me!</button>
+    </div>
+  );
+};
 
 export default Introduction;
